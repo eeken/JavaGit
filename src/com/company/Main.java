22c6dd9878e630e16842982aefca849aaee17d3e
Ola Wiebe <eeken07@gmail.com>
2019-10-09 13:35:36 +0200

    again2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello\n");
+        System.out.print("hello\n");
+        System.out.print("hello\n");
     }
 }
